e98f2c42ea6244cff9c3e06d18f24f168e5c7029
noleynik29 <noleynik29@gmail.com>
2025-11-14 18:12:05 +0200

    Task done

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -27,10 +27,6 @@ public class Actor implements Cloneable {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -29,10 +29,6 @@ public class Movie implements Cloneable {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getTitle() {
         return title;
     }
